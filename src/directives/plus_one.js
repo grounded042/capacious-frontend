4ab26f654ad76559d515d0991baa2aff52e063c5
Jon Carl <joncarl42@gmail.com>
2015-11-16 20:59:10 -0500

    disable save & continue on plus one
    when the plus one is attending and their first or last name has not been filled in, disable the save & continue button

--- src/directives/plus_one.js
+++ src/directives/plus_one.js
@@ -6,6 +6,7 @@ export default function plusOne() {
       getLocClass: '&',
       willAttendFn: '&',
       willNotAttendFn: '&',
+      attending: '=',
       firstName: '=',
       lastName: '=',
       backFn: '&',
