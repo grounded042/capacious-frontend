2532e5fb74bb93863e1723dc119621947d3f0ad5
Jon Carl <joncarl42@gmail.com>
2015-11-16 21:33:27 -0500

    remove unneeded data

--- src/directives/plus_one.js
+++ src/directives/plus_one.js
@@ -6,7 +6,6 @@ export default function plusOne() {
       getLocClass: '&',
       willAttendFn: '&',
       willNotAttendFn: '&',
-      attending: '=',
       firstName: '=',
       lastName: '=',
       disableSaveFn: '&',
